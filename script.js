2825d01aa839aff466a8fb3b4d469f1b22ba8e2a
hekin43 <karima.yamaleeva@mail.ru>
2023-11-15 22:04:38 +0300

    feat: add link to main button

---
+++ script.js
@@ -0,0 +1,19 @@
+const expertBtn = document.querySelector(".buttonExp");
+const popUpWindow = document.querySelector(".popUpWindow");
+const cross = document.querySelector(".union");
+
+expertBtn.addEventListener("click", function() {
+	popUpWindow.classList.toggle('hidden');
+})
+
+cross.addEventListener("click", function() {
+	popUpWindow.classList.add('hidden');
+})
+
+document.addEventListener("click", (e) => {
+	const clickBtn = e.composedPath().includes(expertBtn);
+	const clickWindow = e.composedPath().includes(popUpWindow);
+	if ( !(clickBtn || clickWindow)) {
+		popUpWindow.classList.add('hidden');
+	}	
+})
